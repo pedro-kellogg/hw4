e0c6955142bb007e3a81920d2f231773e6025e30
pedro-kellogg <pedro.albuquerque@kellogg.northwestern.edu>
2026-06-07 22:04:04 +0000

    optional image

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,18 @@
 class PlacesController < ApplicationController
 
   def index
+    @current_user = User.find_by({ "id" => session["user_id"] })
     @places = Place.all
   end
 
   def show
     @place = Place.find_by({ "id" => params["id"] })
-    @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    @current_user = User.find_by({ "id" => session["user_id"] })
+    if @current_user != nil
+      @entries = Entry.where({ "place_id" => @place["id"], "user_id" => @current_user["id"] })
+    else
+      @entries = []
+    end
   end
 
   def new
